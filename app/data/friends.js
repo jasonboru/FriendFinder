987395dfc866ed0a908622f9342e96f7ee399b81
jasonboru <jasonboru@gmail.com>
2017-04-22 20:48:52 -0400

    initial friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,90 @@
 var friendsList = [
-{
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  scores: [
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-}
+  {
+    "name": "Ned Stark",
+    "photo": "/images/ned-stark.jpg",
+    "scores": [3, 3, 5, 2, 5, 3, 5, 2, 5, 1]
+  },
+  {
+    "name": "Oberyn Martel ",
+    "photo": "/images/oberyn-martel.jpg",
+    "scores": [2, 1, 5, 1, 5, 5, 4, 3, 2, 5]
+  },
+  {
+    "name": "Melisandre",
+    "photo": "images/melisandre.jpg",
+    "scores": [5, 5, 5, 5, 2, 5, 1, 5, 3, 2]
+  },
+  {
+    "name": "Samwell Tarly",
+    "photo": "images/samwell-tarly.jpg",
+    "scores": [1, 5, 3, 2, 5, 1, 2, 4, 5, 1]
+  },
+  {
+    "name": "Theon Greyjoy",
+    "photo": "images/theon-greyjoy.jpg",
+    "scores": [5, 1, 1, 2, 2, 5, 4, 1, 3, 4]
+  },
+  {
+    "name": "Jamie Lannister",
+    "photo": "images/jamie-lannister.jpg",
+    "scores": [2, 2, 3, 2, 1, 5, 4, 1, 2, 2]
+  },
+  {
+    "name": "Bronn",
+    "photo": "images/bronn.jpg",
+    "scores": [2, 1, 5, 1, 4, 5, 2, 1, 3, 3]
+  },
+  {
+    "name": "Littlefinger",
+    "photo": "images/littlefinger.jpg",
+    "scores": [5, 5, 4, 5, 2, 3, 2, 1, 2, 4]
+  },
+  {
+    "name": "The Hound",
+    "photo": "images/the-hound.jpg",
+    "scores": [1, 1, 5, 2, 2, 5, 1, 3, 4, 1]
+  },
+  {
+    "name": "Varys",
+    "photo": "images/varys.jpg",
+    "scores": [5, 5, 5, 5, 1, 1, 1, 1, 2, 4]
+  },
+  {
+    "name": "Jon Snow",
+    "photo": "images/jon-snow.jpg",
+    "scores": [2, 1, 4, 1, 5, 3, 5, 2, 5, 1]
+  },
+  {
+    "name": "Sansa Stark",
+    "photo": "images/sansa-stark.jpg",
+    "scores": [3, 4, 3, 3, 3, 3, 4, 2, 2, 4]
+  },
+  {
+    "name": "Joffrey Baratheon",
+    "photo": "images/joffrey-baratheon.jpg",
+    "scores": [5, 3, 1, 2, 1, 5, 1, 1, 1, 4]
+  },
+  {
+    "name": "Cersei Lannister",
+    "photo": "images/cersei-lannister.jpg",
+    "scores": [5, 5, 2, 3, 1, 5, 4, 1, 1, 2]
+  },
+  {
+    "name": "Arya Stark",
+    "photo": "images/arya-stark.jpg",
+    "scores": [2, 2, 5, 3, 4, 4, 4, 1, 4, 1]
+  },
+  {
+    "name": "Tyrion Lannister",
+    "photo": "images/tyrion-lannister.jpg",
+    "scores": [4, 5, 4, 3, 2, 4, 1, 2, 5]
+  },
+  {
+    "name": "Daenerys Targaryen",
+    "photo": "images/daenerys-targeryen.jpg",
+    "scores": [5, 3, 5, 4, 5, 5, 2, 2, 1, 3]
+  }
 ];
+
 
 module.exports = friendsList;
